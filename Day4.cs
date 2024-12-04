a654f0a017ad5ce34b38cc4e0de3c88f1f902ecb
bjartedn <bjarte@wolftech.no>
2024-12-04 07:21:27 +0100

    Day 4 cleaner

--- Day4.cs
+++ Day4.cs
@@ -21,77 +21,38 @@ public class Day4
         var rows = matrix.GetLength(0);
         var cols = matrix.GetLength(1);
 
-
-        //left right
         var count = 0;
-        for (int row = 0; row < rows; row++)
-        {
-            for (int col = 0; col < cols - 3; col++)
-            {
-                var ok = matrix[row, col] == 'X' && matrix[row, col + 1] == 'M' && matrix[row, col + 2] == 'A' &&
-                         matrix[row, col + 3] == 'S';
-                if (ok)
-                    count++;
-
-                var ok2 = matrix[row, col] == 'S' && matrix[row, col + 1] == 'A' && matrix[row, col + 2] == 'M' &&
-                          matrix[row, col + 3] == 'X';
-                if (ok2)
-                    count++;
-            }
-        }
-
-        //top down
-        for (int row = 0; row < rows - 3; row++)
+        for (var row = 0; row < rows; row++)
         {
             for (int col = 0; col < cols; col++)
             {
-                var ok = matrix[row, col] == 'X' && matrix[row + 1, col] == 'M' && matrix[row + 2, col] == 'A' &&
-                         matrix[row + 3, col] == 'S';
-                if (ok)
-                    count++;
-
-                var ok2 = matrix[row, col] == 'S' && matrix[row + 1, col] == 'A' && matrix[row + 2, col] == 'M' &&
-                          matrix[row + 3, col] == 'X';
-                if (ok2)
-                    count++;
+                //left right
+                var ok1 =  (col + 3 < cols) && matrix[row, col] == 'X' && matrix[row, col + 1] == 'M' && matrix[row, col + 2] == 'A' && matrix[row, col + 3] == 'S';
+                var ok2 =  (col + 3 < cols) && matrix[row, col] == 'S' && matrix[row, col + 1] == 'A' && matrix[row, col + 2] == 'M' && matrix[row, col + 3] == 'X';
+                
+                //top down
+                var ok3 = (row + 3 < rows) && matrix[row, col] == 'X' && matrix[row + 1, col] == 'M' && matrix[row + 2, col] == 'A' && matrix[row + 3, col] == 'S';
+                var ok4 = (row + 3 < rows) && matrix[row, col] == 'S' && matrix[row + 1, col] == 'A' && matrix[row + 2, col] == 'M' && matrix[row + 3, col] == 'X';
+            
+                //diag 1
+                var ok5 =  (col + 3 < cols) && (row + 3 < rows) && matrix[row, col] == 'X' && matrix[row + 1, col + 1] == 'M' &&  matrix[row + 2, col + 2] == 'A' && matrix[row + 3, col + 3] == 'S';
+                var ok6 =  (col + 3 < cols) && (row + 3 < rows) && matrix[row, col] == 'S' && matrix[row + 1, col + 1] == 'A' &&  matrix[row + 2, col + 2] == 'M' && matrix[row + 3, col + 3] == 'X';
+               
+                //diag 2
+                var ok7 =  (col + 3 < cols) && (row  - 3 >= 0) && matrix[row, col] == 'X' && matrix[row - 1, col + 1] == 'M' && matrix[row - 2, col + 2] == 'A' && matrix[row - 3, col + 3] == 'S';
+                var ok8 =  (col + 3 < cols) && (row  - 3 >= 0) && matrix[row, col] == 'S' && matrix[row - 1, col + 1] == 'A' && matrix[row - 2, col + 2] == 'M' && matrix[row - 3, col + 3] == 'X';
+              
+                if (ok1) count++;
+                if (ok2) count++;
+                if (ok3) count++;
+                if (ok4) count++;
+                if (ok5) count++;
+                if (ok6) count++;
+                if (ok7) count++;
+                if (ok8) count++;
             }
         }
-
-        //a diagonal starts at every matrix cell
-        for (var row = 0; row < rows - 3; row++)
-        {
-            for (int col = 0; col < cols - 3; col++)
-            {
-                var ok = matrix[row, col] == 'X' && matrix[row + 1, col + 1] == 'M' &&
-                         matrix[row + 2, col + 2] == 'A' && matrix[row + 3, col + 3] == 'S';
-                if (ok)
-                    count++;
-
-                var ok2 = matrix[row, col] == 'S' && matrix[row + 1, col + 1] == 'A' &&
-                          matrix[row + 2, col + 2] == 'M' && matrix[row + 3, col + 3] == 'X';
-                if (ok2)
-                    count++;
-            }
-        }
-
-        for (var row = 3; row < rows; row++)
-        {
-            for (int col = 0; col < cols - 3; col++)
-            {
-                var ok = matrix[row, col] == 'X' && matrix[row - 1, col + 1] == 'M' &&
-                         matrix[row - 2, col + 2] == 'A' && matrix[row - 3, col + 3] == 'S';
-                if (ok)
-                    count++;
-
-                var ok2 = matrix[row, col] == 'S' && matrix[row - 1, col + 1] == 'A' &&
-                          matrix[row - 2, col + 2] == 'M' && matrix[row - 3, col + 3] == 'X';
-                if (ok2)
-                    count++;
-            }
-        }
-
-        Console.WriteLine($"Count is {count}"); //2534
-
+        Console.WriteLine($"XMAS count is {count}"); //2476
 
         //December 4 Part 2
         count = 0;
